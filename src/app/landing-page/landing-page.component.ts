b7b876284931bc27e857caba6e66d0e44a60e6e0
EmiKilic <emirhan_kilic@outlook.de>
2025-02-03 02:40:12 +0100

    landing page done

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
--- src/app/leistungen/leistungen.component.ts
+++ src/app/leistungen/leistungen.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-leistungen',
-  imports: [],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule, MatButtonModule],
   templateUrl: './leistungen.component.html',
   styleUrl: './leistungen.component.scss'
 })
