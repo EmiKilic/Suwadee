c3b36702f80dd842d53470348d4f63fd8b8a8681
EmiKilic <emirhan_kilic@outlook.de>
2025-02-12 12:51:45 +0100

    responsiveness header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav } from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule],
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatMenuModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
